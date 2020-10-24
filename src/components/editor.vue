e05db464fdc94b07615fce8dc5dd60045df88a8b
hutianhao <812510003@qq.com>
2020-10-24 15:29:11 +0800

    feat: 失焦保存选区

--- src/components/editor.vue
+++ src/components/editor.vue
@@ -2,11 +2,15 @@
   <div class="editor">
     <div>
       <button @click="handlerBold">加粗</button>
-      <button @click="handlerFont">修改字体</button>
-      <button @click="handlerColor">修改颜色</button>
-      <input type="text" placeholder="输入修改的颜色" v-model="color">
+      <button @click="handlerFont('h1')">一级标题</button>
+      <button @click="handlerFont('h2')">二级标题</button>
+      <button @click="handlerFont('h3')">三级标题</button>
+      <input type="color" @change="handlerColor($event.target.value)" class="color-picker">
     </div>
-    <div class="editor-content" contenteditable="true"></div>
+    <div class="editor-content" contenteditable="true" 
+      @keydown.enter.prevent="preventEnter($event)"
+      @blur="saveRange"
+    ></div>
   </div>
 </template>
 
@@ -17,23 +21,46 @@ import { Component, Prop, Watch, Emit } from "vue-property-decorator";
 @Component
 export default class Editor extends Vue {
   private msg: string = '';
-  private color: string = '';
+  private range: Range | null = null;
 
   private handlerBold(): void {
-    document.execCommand('bold');
+    this.doCommand('bold', '');
   }
 
-  private handlerFont(): void {
-    document.execCommand('formatBlock', false, '<h1>');
+  private handlerFont(value: string): void {
+    this.doCommand('formatBlock', value);
   }
 
-  private handlerColor(): void {
-    if (!this.color) {
-      alert('请输入修改的颜色');
+  private handlerColor(value: string): void {
+    this.doCommand('ForeColor', value);
+  }
+
+  private doCommand(command: string, value: string): void {
+    this.restoreRange();
+    document.execCommand(command, false, value);
+  }
+  
+  /**
+   * 保存选区
+   */
+  private saveRange() {
+    const selection = document.getSelection() as Selection;
+    if (selection && selection.rangeCount === 0) {
       return;
     }
-    document.execCommand('ForeColor', false, this.color);
+    this.range = selection.getRangeAt(0);
+  }
 
+  public restoreRange(): void {
+    const selection = document.getSelection() as Selection;
+    if (selection && this.range) {
+      selection.removeAllRanges();
+      selection.addRange(this.range);
+    }
+  }
+
+  preventEnter (e: Event) {
+    document.execCommand("insertHTML", false, `<br></br>`);
   }
 }
 </script>
@@ -43,5 +70,11 @@ export default class Editor extends Vue {
     width: 500px;
     height: 200px;
     border: 1px solid #333;
+  }
+  .editor-content:focus {
+    outline: none;
+  }
+  .color-picker {
+    vertical-align: middle;
   }
 </style>
